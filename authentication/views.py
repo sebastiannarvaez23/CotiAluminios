ca610d5f151f4fffa423e92d360778c66ba7cee7
Sebastian Narvaez Lopera <narvaezsebas8@gmail.com>
2023-04-13 15:49:56 -0500

    Template del login terminado

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+class LoginTemplateView(TemplateView):
+    template_name = "login.html"
