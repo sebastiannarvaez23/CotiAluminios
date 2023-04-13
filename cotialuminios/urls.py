012b3dd0c1b08f292b3fb9b73091925dd0e61339
Sebastian Narvaez Lopera <narvaezsebas8@gmail.com>
2023-04-13 15:36:08 -0500

    Integrando la app authentication para el manejo del registro y la autenticación

--- cotialuminios/urls.py
+++ cotialuminios/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('window/', include('window.urls')),
+    path('auth/', include('authentication.urls')),
 ]
