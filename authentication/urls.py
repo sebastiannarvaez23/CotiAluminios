1d5a80a7b953a714d5f8fc450a1654720011aedd
Sebastian Narvaez Lopera <narvaezsebas8@gmail.com>
2023-04-13 22:24:55 -0500

    Implementacion de navbar en el template base

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from authentication import views
 
 urlpatterns = [
-    path('login/', views.LoginTemplateView.as_view(), name='quote'),
+    path('login/', views.LoginTemplateView.as_view(), name='login'),
 ]
