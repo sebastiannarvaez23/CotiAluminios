891a34384b68b3a8b5335a644a3fb8b39f7bc10e
Sebastian Narvaez Lopera <narvaezsebas8@gmail.com>
2023-04-16 22:40:53 -0500

    Optimizando los test utilizando el decorador pytest.fixture

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,38 +1,28 @@
 import pytest
 
-@pytest.mark.django_db
-def test_user_creation():
+@pytest.fixture
+def user_creation():
     from django.contrib.auth.models import User
-
-    user = User.objects.create_user(
+    return User(
         username = 'testusername',
         email = 'test@test.com',
         password = 'TestPass12345'
     )
 
-    assert user.username == 'testusername'
+@pytest.mark.django_db
+def test_user_creation(user_creation):
+    user_creation.save()
+    assert user_creation.username == 'testusername'
 
 @pytest.mark.django_db
-def test_superuser_creation():
-    from django.contrib.auth.models import User
-
-    user = User.objects.create_superuser(
-        username = 'testusername',
-        email = 'test@test.com',
-        password = 'TestPass12345'
-    )
-
-    assert user.is_superuser
+def test_superuser_creation(user_creation):
+    user_creation.is_superuser = True
+    user_creation.is_staff = True
+    user_creation.save()
+    assert user_creation.is_superuser
 
 @pytest.mark.django_db
-def test_staff_user_creation():
-    from django.contrib.auth.models import User
-
-    user = User.objects.create_user(
-        username = 'testusername',
-        email = 'test@test.com',
-        password = 'TestPass12345',
-        is_staff = True
-    )
-
-    assert user.is_staff
+def test_staff_user_creation(user_creation):
+    user_creation.is_staff = True
+    user_creation.save()
+    assert user_creation.is_staff
