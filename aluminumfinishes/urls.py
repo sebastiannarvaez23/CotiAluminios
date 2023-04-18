65513786a90a06014dc9f69a9553f3291648639f
Sebastian Narvaez Lopera <narvaezsebas8@gmail.com>
2023-04-18 15:13:20 -0500

    Contruccion de vistas para crear y eliminar acabados en aluminio

--- aluminumfinishes/urls.py
+++ aluminumfinishes/urls.py
@@ -3,4 +3,6 @@ from aluminumfinishes import views
 
 urlpatterns = [
     path('list/', views.AluminumFinishesTemplateView.as_view(), name="aluminumfinishes"),
+    path('create/', views.StylesWindowCreateView.as_view(), name="aluminumfinishescreate"),
+    path('delete/<int:pk>/', views.StylesWindowDeleteView.as_view(), name="aluminumfinishesdelete"),
 ]
--- aluminumfinishes/views.py
+++ aluminumfinishes/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
 from django.utils.decorators import method_decorator
+from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, DeleteView
 from aluminumfinishes.models import AluminumFinishes
 
 # Create your views here.
@@ -12,3 +15,28 @@ class AluminumFinishesTemplateView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['aluminum_finishes'] = AluminumFinishes.objects.all()
         return context
+
+@method_decorator(login_required, name='dispatch')
+class StylesWindowCreateView(CreateView):
+    model = AluminumFinishes
+    template_name = "aluminum_finishes.html"
+    fields = ['name', 'price']
+    
+    def get_success_url(self):
+        return reverse_lazy('aluminumfinishes')
+    
+    def form_valid(self, form):
+        name = self.request.POST.get('name')
+        price = self.request.POST.get('price')
+        # Pendiente terminar esta validación
+        if name == None or price == None: return HttpResponse(status=400)
+        aluminum_finishes = AluminumFinishes(name=name, price=price)
+        aluminum_finishes.save()
+
+        return HttpResponseRedirect(redirect_to=self.get_success_url())
+
+@method_decorator(login_required, name='dispatch')
+class StylesWindowDeleteView(DeleteView):
+    model = AluminumFinishes
+    template_name = 'aluminum_finishes_conf_delete.html'
+    success_url = reverse_lazy('aluminumfinishes')
